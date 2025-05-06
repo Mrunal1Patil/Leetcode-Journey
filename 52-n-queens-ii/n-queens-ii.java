03ba9363c0da3d181f4856fd73d5e5d94a03ab71
Mrunal1Patil <161746327+Mrunal1Patil@users.noreply.github.com>
2025-05-06 12:03:42 -0400

    Time: 1 ms (77.85%) | Memory: 40.4 MB (63.11%) - LeetSync

---
+++ 52-n-queens-ii/n-queens-ii.java
@@ -0,0 +1,46 @@
+
+class Solution {
+    private int count = 0;
+
+    public int totalNQueens(int n) {
+        char[][] board = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(board[i], '.');
+        }
+        solve(0, board);
+        return count;
+    }
+
+    private void solve(int row, char[][] board) {
+        if (row == board.length) {
+            count++;
+            return;
+        }
+
+        for (int col = 0; col < board.length; col++) {
+            if (isSafe(board, row, col)) {
+                board[row][col] = 'Q';
+                solve(row + 1, board);
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    private boolean isSafe(char[][] board, int row, int col) {
+        int n = board.length;
+
+        for (int i = 0; i < row; i++) {
+            if (board[i][col] == 'Q') return false;
+        }
+
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') return false;
+        }
+
+        for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+            if (board[i][j] == 'Q') return false;
+        }
+
+        return true;
+    }
+}
