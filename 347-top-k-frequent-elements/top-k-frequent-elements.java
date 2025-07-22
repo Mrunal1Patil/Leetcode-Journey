cc838c9157e6f55cb1bf41d28b735d0b2400a868
Mrunal1Patil <161746327+Mrunal1Patil@users.noreply.github.com>
2025-07-22 10:52:04 -0400

    Time: 15 ms (38.25%) | Memory: 45.7 MB (86.30%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,22 +1,33 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
 
-        Map<Integer, Integer > map = new HashMap<>();
-        for(int n: nums){
-            map.put(n , map.getOrDefault(n,0)+1);
-        }
-        Queue<Integer> heap = new PriorityQueue<>((n1,n2) -> map.get(n1) - map.get(n2));
-        for(int n : map.keySet()){
-            heap.offer(n);
-            if(heap.size()>k){
-                heap.poll();
+        Map<Integer, Integer> freqMap = new HashMap<>();
+        for (int num : nums) {
+            if (freqMap.containsKey(num)) {
+                freqMap.put(num, freqMap.get(num) + 1);
+            } else {
+                freqMap.put(num, 1);
             }
         }
-        int [] result = new int[k];
-        for(int i=k-1;i>=0;i--){
-            result[i]=heap.poll();
+
+    
+        PriorityQueue<Map.Entry<Integer, Integer>> minHeap =
+            new PriorityQueue<>((a, b) -> a.getValue() - b.getValue());
+
+        for (Map.Entry<Integer, Integer> entry : freqMap.entrySet()) {
+            minHeap.add(entry);
+            if (minHeap.size() > k) {
+                minHeap.poll();
+            }
         }
+
+
+        int[] result = new int[k];
+        int i = 0;
+        while (!minHeap.isEmpty()) {
+            result[i++] = minHeap.poll().getKey();
+        }
+
         return result;
     }
-
 }
