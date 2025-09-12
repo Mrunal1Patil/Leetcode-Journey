9d5ab0e9933d09e9af781ea8f5eeb6d85100b3f8
Mrunal1Patil <161746327+Mrunal1Patil@users.noreply.github.com>
2025-09-12 15:17:19 -0400

    Time: 151 ms (39.31%) | Memory: 151.1 MB (67.51%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -1,36 +1,36 @@
 class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {  
         Map<Integer, List<Integer>> map = new HashMap<>();
-     for (int i = 0; i < n; i++) {
-            map.put(i, new ArrayList<>());
+
+        for(int i=0;i<n;i++){
+            map.put(i,new ArrayList<>());
+        }
+        
+        for(int[] edge: edges){
+            int u = edge[0];
+            int v = edge[1];
+            map.get(u).add(v);
+            map.get(v).add(u);
         }
 
-        for (int[] edge : edges) {
-            map.get(edge[0]).add(edge[1]);
-            map.get(edge[1]).add(edge[0]);
-        }
-
- 
-        Set<Integer> visited = new HashSet<>();
         Queue<Integer> queue = new LinkedList<>();
+        Set<Integer> set = new HashSet<>();
 
-        visited.add(source);
-        queue.add(source);
+        queue.offer(source);
+        set.add(source);
 
-        while (!queue.isEmpty()) {
+        while(!queue.isEmpty()){
             int node = queue.poll();
-            if (node == destination) {
+            if(node==destination){
                 return true;
             }
-
-            for (int neighbor : map.get(node)) {
-                if (!visited.contains(neighbor)) {
-                    queue.offer(neighbor);
-                    visited.add(neighbor);
+            for(int nieghbor : map.get(node)){
+                if(!set.contains(nieghbor)){
+                    queue.offer(nieghbor);
+                    set.add(nieghbor);
                 }
             }
-        }
-
-        return false;
+                    }
+                    return false;
     }
 }
