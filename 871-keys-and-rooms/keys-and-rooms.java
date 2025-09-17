7d302288e2f57cb32ec0e0b0acbaad43006d9ed1
Mrunal1Patil <161746327+Mrunal1Patil@users.noreply.github.com>
2025-09-17 08:36:08 -0400

    Time: 3 ms (21.26%) | Memory: 44.7 MB (24.69%) - LeetSync

--- 871-keys-and-rooms/keys-and-rooms.java
+++ 871-keys-and-rooms/keys-and-rooms.java
@@ -1,23 +1,22 @@
 class Solution {
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
-        
-       Queue<Integer> queue = new LinkedList<>();
-       Set<Integer> set = new HashSet<>();
+      
+      Set<Integer> set = new HashSet<>();
+      Queue<Integer> queue = new LinkedList<>();
 
-       queue.add(0);
-       set.add(0);
+      queue.offer(0);
+      set.add(0);
 
-       while(!queue.isEmpty()){
-        int room = queue.poll();
+      while(!queue.isEmpty()){
+        int roomkey = queue.poll();
 
-        for(int key : rooms.get(room)){
-            if(!set.contains(key)){
-                queue.offer(key);
-                set.add(key);
+        for(int room : rooms.get(roomkey)){
+            if(!set.contains(room)){
+                set.add(room);
+                queue.offer(room);
             }
         }
-
-       }
-       return rooms.size() == set.size();
+      }
+        return rooms.size()==set.size();
     }
 }
